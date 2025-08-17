4acb660b6d6da9747dbb2d3cb87a0377a71d824b
YinChangQing <yinchangqing@pinduoduo.com>
2025-08-17 21:44:49 +0800

    feat: 添加 LSP 插件

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,14 @@
+-- =============================================================================
+--  LSP
+-- =============================================================================
+
+return {
+  { "mason-org/mason.nvim", opts = {} },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+  },
+}
